ffcba0d4dd693305b7b0318a77ccefcff706a400
fatimazahraben-alla <fatisafae33@gmail.com>
2024-04-19 16:35:14 +0100

    Update code

--- src/main/java/ma/digital/prospace/config/SecurityConfiguration.java
+++ src/main/java/ma/digital/prospace/config/SecurityConfiguration.java
@@ -7,29 +7,34 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 
 @Configuration
-
+@EnableWebSecurity
 public class SecurityConfiguration {
 
-
     @Bean
-
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest()
-
-                        .permitAll())
-
-                .csrf(AbstractHttpConfigurer::disable);
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> {
+                    CorsConfiguration corsConfig = new CorsConfiguration();
+                    corsConfig.applyPermitDefaultValues();
+                    corsConfig.addAllowedMethod("DELETE");
+                    corsConfig.addAllowedMethod("PUT");
+                    corsConfig.addAllowedMethod("GET");
+                    corsConfig.addAllowedMethod("POST");
+                    corsConfig.setMaxAge(3600L);
+                    cors.configurationSource(request -> corsConfig);
+                })
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll());
 
         return http.build();
-
-
     }
 
 }
