e970d1f6ec24d0686285cdfb00eccd09757c077e
BOUTAIB <mohamedmer220@gmail.com>
2024-04-20 06:48:58 +0100

    update

--- src/main/java/ma/digital/prospace/config/SecurityConfiguration.java
+++ src/main/java/ma/digital/prospace/config/SecurityConfiguration.java
@@ -2,13 +2,19 @@ package ma.digital.prospace.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class  SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -16,7 +22,9 @@ public class SecurityConfiguration {
 
                 .csrf(csrf -> csrf.disable()) // Désactive CSRF
                 .authorizeRequests(authz -> authz.anyRequest().permitAll()); // Autorise toutes les requêtes
+        // Active la configuration CORS
 
         return http.build();
     }
 }
+
