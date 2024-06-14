5c9771876b761bad96b5be90a8c865ef2cec0168
BOUTAIB <mohamedmer220@gmail.com>
2024-06-14 21:15:49 +0100

    update

--- src/main/java/ma/digital/prospace/service/dto/EntrepriseDTO2.java
+++ src/main/java/ma/digital/prospace/service/dto/EntrepriseDTO2.java
@@ -1,12 +1,17 @@
 package ma.digital.prospace.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
-
+/**
+ * A DTO for the {@link ma.digital.prospace.domain.Entreprise} entity.
+ */
 @Component
 public class EntrepriseDTO2 implements Serializable {
+    @JsonProperty("id")
     private String id;
+    @JsonProperty("name")
     private String etat;
     public String getId() {
         return id;
@@ -22,5 +27,13 @@ public class EntrepriseDTO2 implements Serializable {
 
     public void setEtat(String etat) {
         this.etat = etat;
+    }
+
+    @Override
+    public String toString() {
+        return "EntrepriseDTO2{" +
+                "id='" + id + '\'' +
+                ", etat='" + etat + '\'' +
+                '}';
     }
 }
